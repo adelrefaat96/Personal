a06d48418b7c51d122acc7144a040888ab38bdef
adelrefaat96 <160245945+adelrefaat96@users.noreply.github.com>
2024-08-02 20:53:13 +0200

    Add files via upload

---
+++ Calculate_area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+constexpr double pi = 3.14;
+
+int main()
+{
+    int r; 
+    int Area_of_circle;
+cout << "Enter radius in cm = " << endl;
+cin >> r;
+Area_of_circle = r*r*pi;
+cout << "Area in cm^2 = " << Area_of_circle << endl;
+
+}
---
+++ change.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+
+int main()
+{
+  cout << "Distance in Miles = ";
+  int x;
+  cin >> x;
+  float y;
+  y = x*1.609;
+  cout << "Distance in kilometers = " << y << endl ;
+  
+}
